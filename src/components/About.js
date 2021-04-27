e6111a98bd7a136d1f1aacd3c54646f3e4230b35
Yuri Nguyen <yuringuyen312@gmail.com>
2021-04-26 21:27:56 -0400

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,8 @@
 import React from "react";
+import {image} from "../data/user";
 
 function About() {
+  
   return (
     <div id="about">
       <h2>About Me</h2>
@@ -9,3 +11,4 @@ function About() {
     </div>
   );
 }
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
 function App() {
   return (
     <div>
-      <NavBar />
+      <NavBar/>
       <Home />
       <About />
     </div>
